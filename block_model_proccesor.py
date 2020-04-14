2674eee9056c995f32cc8f20a6005f03b5a06dff
Vicente Correa <vicorrea@miuandes.cl>
2020-04-14 13:56:38 -0400

    read file and load to sqlite3 db

--- block_model_proccesor.py
+++ block_model_proccesor.py
@@ -1,9 +1,27 @@
-import pandas as pd
+import sqlite3
 
 def load_block_file():
     file_path = input("File path: ")
     try:
-        data = pd.read_csv(file_path, delim_whitespace=True)
+        conn = sqlite3.connect(DB_NAME)
+        try:
+            conn.execute("CREATE TABLE BLOCK (ID INT PRIMARY KEY NOT NULL, X INT NOT NULL, Y INT NOT NULL, Z INT NOT NULL, VALUE INT NOT NULL, TON FLOAT NOT NULL, DESTINATION INT NOT NULL, AU FLOAT NOT NULL);")
+            data = open(file_path)
+            for line in data:
+                columns = line.split()
+                id = int(columns[0])
+                x = int(columns[1])
+                y = int(columns[2])
+                z = int(columns[3])
+                value = int(columns[4])
+                ton = float(columns[5])
+                destination = int(columns[6])
+                au = float(columns[7])
+                conn.execute("INSERT INTO BLOCK (ID, X, Y, Z, VALUE, TON, DESTINATION, AU) VALUES ({}, {}, {}, {}, {}, {}, {}, {})".format(id, x, y, z, value, ton, destination, au))
+            conn.commit()
+        except:
+            print("The file has already been read")
+        conn.close()
 
     except:
         print("File not found")
@@ -11,10 +29,22 @@ def load_block_file():
     return
 
 def query_console():
+    conn = sqlite3.connect("block_model.db")
+    cursor = conn.execute("SELECT * from BLOCK")
+
+    # testing
+
+    c = 0
+    for row in cursor:
+        print(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7])
+        if c > 200:
+            break
+        c += 1
+    conn.close()
     #TODO: IMPLEMENT FUNCTIONALITY
     return
 VALID_OPTIONS = ["0", "1", "2"]
-
+DB_NAME = "block_model.db"
 def main_menu():
     while True:
         print("What do you want to do \n"
