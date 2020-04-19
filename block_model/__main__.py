761a5d56b0736f74a7d307ec536a321f406a23b4
ignaciofigueroa <ijfigueroa@miuandes.cl>
2020-04-18 20:40:23 -0400

    feat(load_block_model): create json data file if does not exists

--- block_model/__main__.py
+++ block_model/__main__.py
@@ -1,10 +1,20 @@
 import sys
 import block_model_proccesor
+import query_console
+import json 
+import os
+LOADED_MODELS_INFORMATION_FILE_NAME = "models_information.json"
+
+def check_neccesary_files_existence():
+    if not os.path.isfile(LOADED_MODELS_INFORMATION_FILE_NAME):
+        with open(LOADED_MODELS_INFORMATION_FILE_NAME, "w+") as f:
+            json.dump({}, f, sort_keys=True)
 
 def main(args=None):
     """The main routine."""
     if args is None:
         args = sys.argv[1:]
+    check_neccesary_files_existence()
     query_console.main_menu()
     # Do argument parsing here (eg. with argparse) and anything else
     # you want your project to do.
