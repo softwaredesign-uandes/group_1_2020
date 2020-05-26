8b02f1b89f4c811b17f7b28404ceea243e70c834
ignaciofigueroa <ijfigueroa@miuandes.cl>
2020-05-26 16:28:14 -0400

    Endpoints structure

--- api/__main__.py
+++ api/__main__.py
@@ -6,6 +6,13 @@ app = Flask(__name__)
 def Index():
     return 'Hello World'
 
+@app.route('/api/block_models/', methods=['GET'])
+def get_block_models_names():
+    return 'block models'
+
+@app.route('/api/block_models/<name>/blocks/', methods=['GET'])
+def get_block_model_blocks(name=None):
+    return name
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
