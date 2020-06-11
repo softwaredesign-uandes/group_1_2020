b3ad32e824e66c59808778c21ab76dac6bc6923b
ignaciofigueroa <ijfigueroa@miuandes.cl>
2020-06-11 14:18:18 -0400

    feat(): -Reblock endpoint

--- api/__main__.py
+++ api/__main__.py
@@ -1,6 +1,7 @@
-from flask import Flask, jsonify, Response
+from flask import Flask, jsonify, Response, request
 import json
-import block_model_proccesor
+import block_model_proccesor, api_verification, load_block_model
+from constants import LOADED_MODELS_INFORMATION_FILE_NAME
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,12 +14,20 @@ def get_block_models_names():
     response.headers["Access-Control-Allow-Origin"] = "*"
     return response
 
-
 @app.route('/api/block_models/<name>/blocks/', methods=['GET'])
 def get_block_model_blocks(name=None):
     response = Response(json.dumps(block_model_proccesor.get_block_list(name)))
     response.headers["Access-Control-Allow-Origin"] = "*"
     return response
+
+@app.route('/api/block_models/<name>/reblock', methods=['POST'])
+def reblock_block_model(name=None, json_file_name=LOADED_MODELS_INFORMATION_FILE_NAME):
+    data = request.get_json()
+    valid_information = api_verification.verificate_reblock_information(data, name, json_file_name)
+    if valid_information:
+        block_model = load_block_model.get_block_model_object(name)
+        block_model.reblock(data["rx"], data["ry"], data["rz"], data["continuous_attributes"], data["proportional_attributes"], data["categorical_attributes"], data["columns_with_mass"])
+    return
 
 if __name__ == '__main__':
     app.run()
