2c50817ac8084884082abf28ee4ea6716b5ec02e
ignaciofigueroa <ijfigueroa@miuandes.cl>
2020-06-11 17:37:02 -0400

    Reblock endpoint status codes

--- api/__main__.py
+++ api/__main__.py
@@ -4,8 +4,6 @@ import block_model_proccesor, api_verification, load_block_model
 from constants import LOADED_MODELS_INFORMATION_FILE_NAME
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def Index():
     return 'Hello World'
@@ -15,7 +13,7 @@ def get_block_models_names():
     feature_flags_json = get_feature_flags()
     data = block_model_proccesor.get_model_names_to_dictionary()
     if feature_flags_json["restful_response"]:
-        data = {"block_models": data}
+         data = {"block_models": data}
     response = Response(json.dumps(data))
     response.headers["Access-Control-Allow-Origin"] = "*"
     return response
@@ -42,14 +40,25 @@ def get_block_model_blocks_info(name, index):
         response.headers["Access-Control-Allow-Origin"] = "*"
         return response
 
+
 @app.route('/api/block_models/<name>/reblock', methods=['POST'])
 def reblock_block_model(name=None, json_file_name=LOADED_MODELS_INFORMATION_FILE_NAME):
     data = request.get_json()
+    response = Response()
     valid_information = api_verification.verificate_reblock_information(data, name, json_file_name)
     if valid_information:
         block_model = load_block_model.get_block_model_object(name)
-        block_model.reblock(data["rx"], data["ry"], data["rz"], data["continuous_attributes"], data["proportional_attributes"], data["categorical_attributes"], data["columns_with_mass"])
-    return
+        try:
+            block_model.reblock(data["rx"], data["ry"], data["rz"], data["continuous_attributes"],
+                                data["proportional_attributes"],
+                                data["categorical_attributes"], data["columns_with_mass"])
+            response.status_code = 200
+        except:
+            response.status_code = 500
+    else:
+        response.status_code = 400
+    return response
+
 
 def get_feature_flags():
     #TODO change this url to https://dry-brushlands-69779.herokuapp.com/api/feature_flags for the delivery
@@ -57,5 +66,7 @@ def get_feature_flags():
     response = requests.get(feature_flags_service_url)
     feature_flags_json = response.json()
     return feature_flags_json
+
+
 if __name__ == '__main__':
     app.run()
